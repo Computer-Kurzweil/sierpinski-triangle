d89b8c62e68aeb0f44f40ae21b082cc13c37a329
thomaswoehlke <thomas.woehlke@gmail.com>
2022-12-12 01:48:29 +0100

    work

---
+++ src/main/java/org/woehlke/computer/kurzweil/sierpinskitriangle/SimulatedEvolutionApplication.java
@@ -0,0 +1,33 @@
+package org.woehlke.computer.kurzweil.sierpinskitriangle;
+
+import lombok.extern.log4j.Log4j2;
+import org.woehlke.computer.kurzweil.sierpinskitriangle.config.ComputerKurzweilProperties;
+import org.woehlke.computer.kurzweil.sierpinskitriangle.tabs.simulatedevolution.SimulatedEvolutionTab;
+
+/**
+ * Class with main Method for Starting the Desktop Application.
+ *
+ * @see SimulatedEvolutionTab
+ *
+ * &copy; 2006 - 2008 Thomas Woehlke.
+ * http://java.woehlke.org/sierpinskitriangle/
+ * @author Thomas Woehlke
+ */
+@Log4j2
+public class SimulatedEvolutionApplication {
+
+    private SimulatedEvolutionApplication(String configFileName, String jarFilePath) {
+        ComputerKurzweilProperties properties = ComputerKurzweilProperties.propertiesFactory(configFileName, jarFilePath);
+        SimulatedEvolutionTab simulatedEvolutionTab = new SimulatedEvolutionTab(properties);
+    }
+
+    /**
+     * Starting the Desktop Application
+     * @param args CLI Parameter
+     */
+    public static void main(String[] args) {
+        String configFileName = "application.yml";
+        String jarFilePath = "target/sierpinskitriangle.jar";
+        SimulatedEvolutionApplication application = new SimulatedEvolutionApplication(configFileName,jarFilePath);
+    }
+}
